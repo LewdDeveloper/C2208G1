626dcba6e28b9eb142d2d730dbc00d9619c0dcba
imode1337 <imode1337@gmail.com>
2022-10-20 00:50:25 +0700

    funni

--- class_1/e7_4.c
+++ class_1/e7_4.c
@@ -5,14 +5,18 @@ int sumLast3(int[], int);
 
 int main()
 {
-
+    int arr[] = {-1, 2, -3, 4, -5, 6, -7, 8, 9, 10};
+    printf("Sum of last 3 array element = %d", sumLast3(arr, 10));
 }
 
+// incase it wasn't obvious, in C array index starts from 0
+//                           note for people coming from lua background.
 int sumLast3(int arr[], int size)
 {
-    for (int i = 0; i < size; i++)
+    int sum = 0, j = 0;
+    for (int i = (size - 1); i >= 0 && j < 3; i--, j++)
     {
-        /* code */
+        sum += arr[i];
     }
-    
+    return sum;
 }
