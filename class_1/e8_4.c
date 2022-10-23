41e46a9348bbd0edf1d1333a5e356dd7896df950
imode1337 <imode1337@gmail.com>
2022-10-23 18:03:11 +0700

    it's ok

--- class_1/e8_4.c
+++ class_1/e8_4.c
@@ -5,15 +5,15 @@ int countWords(char[]);
 
 int main()
 {
-    printf("Word count : %d\n", countWordsSeperatedBySingleSpace("Sunday, 17th January 2019"));
-    printf("Word count : %d\n", countWords("a               Sunday, 17th               January 2019"));
+    printf("Word count : %d\n", countWordsSeperatedBySingleSpace("a a a a a"));
+    printf("Word count : %d\n", countWords("a           a           a           a\n a"));
 }
 
 int countWordsSeperatedBySingleSpace(char arr[])
 {
     int count = strlen(arr), words = 0;
 
-    for (int cursor = 0; cursor < count; cursor++)
+    for (int cursor = 0; cursor < count;)
     {
         while (!isspace(arr[cursor]))
         {
@@ -32,7 +32,7 @@ int countWordsSeperatedBySingleSpace(char arr[])
 int countWords(char arr[])
 {
     int count = strlen(arr), words = 0;
-    for (int cursor = 0; cursor < count; cursor++)
+    for (int cursor = 0; cursor < count;)
     {
         while (isspace(arr[cursor]))
         {
